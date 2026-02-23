a3c09b0381954b9a3df47520d20b3252bf57d055
Ivan Grga <grga1349@gmail.com>
2026-02-23 23:03:45 +0100

    Fixed CPP flags

--- ci/macos/build.sh
+++ ci/macos/build.sh
@@ -4,7 +4,7 @@ VERSION=$(cat VERSION)
 export CGO_ENABLED=1
 export PKG_CONFIG_PATH="$(brew --prefix opencv)/lib/pkgconfig:$(brew --prefix)/lib/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
 export CGO_CFLAGS="$(pkg-config --cflags opencv4)"
-export CGO_CXXFLAGS="$CGO_CFLAGS"
+export CGO_CXXFLAGS="$CGO_CFLAGS -std=c++17"
 export CGO_LDFLAGS="$(pkg-config --libs opencv4) -Wl,-headerpad_max_install_names"
 export PATH="$(go env GOPATH)/bin:$PATH"
 export MACOSX_DEPLOYMENT_TARGET=14.0
